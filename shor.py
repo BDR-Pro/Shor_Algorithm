29b23f9d77a442b7237a0a324cf6f6ec878281dd
Bader alotaibi <91114465+BDR-Pro@users.noreply.github.com>
2023-10-05 20:02:46 +0300

    find if its a prime or not

--- shor.py
+++ shor.py
@@ -1,17 +1,36 @@
 import math
 
-def shore(n):
+def find_factors(n):
     a = math.ceil(math.sqrt(n))
-    print(a)
-    b2 = a*a - n
-    print(b2)
+    b2 = a * a - n
     while not math.sqrt(b2).is_integer():
         a += 1
-        print(a)
-        b2 = a*a - n
-        print(b2)
+        b2 = a * a - n
     return (a - math.sqrt(b2), a + math.sqrt(b2))
 
-n = 15
-factors = shore(n)
-print(factors)
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+n = 961752149
+factors = find_factors(n)
+
+if is_prime(n):
+    print("The number is prime")
+else:
+    print(factors)
+    res = factors[0] * factors[1] == n
+    if res:
+        print("The factors are correct")
+    else:
+        print("The factors are wrong")
